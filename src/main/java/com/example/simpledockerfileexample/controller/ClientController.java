a67fcb9c6f5488add3875315be12972b8b824158
sirdzzz <chebotarev.da@phystech.edu>
2023-05-11 19:19:08 +0300

    Init commit

---
+++ src/main/java/com/example/simpledockerfileexample/controller/ClientController.java
@@ -0,0 +1,24 @@
+package com.example.simpledockerfileexample.controller;
+
+
+import com.example.simpledockerfileexample.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/client")
+public class ClientController {
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping()
+    public String sayHello(){
+        return clientService.sayHello();
+    }
+}
